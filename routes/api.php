c96b52ae4a5b299a784688e2f1281d629fe89dbd
Tom <55575411+Tom-Crespo@users.noreply.github.com>
2020-05-31 18:43:03 +0200

    usercontroller

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::resource('inicios','InicioController');
 Route::resource('actividads','ActividadController');
 Route::resource('evaluacions','EvaluacionController');
-Route::resource('monitors','MonitorController');
+
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'UserController@details');
+});
